5911c47f7a0436ce1bee92a248fddfff3466ecd7
yebink <yebin825@gmail.com>
2020-04-14 01:20:48 +0900

    생성/수정시간 자동화

--- src/test/java/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/springboot/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,5 +43,28 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime fixedTime = LocalDateTime.of(2019, 4, 15, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("elly")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>> createDate = " + posts.getCreateDate());
+        System.out.println(">>>>>> modifiedDate = " + posts.getModifiedDate());
+
+        assertThat(posts.getCreateDate()).isAfter(fixedTime);
+        assertThat(posts.getModifiedDate()).isAfter(fixedTime);
     }
 }
