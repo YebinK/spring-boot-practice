e0865b7cc7842b3bffb52cdc1251db81d70fbc35
yebink <yebin825@gmail.com>
2020-04-16 00:28:40 +0900

    mustache로 등록 화면 구성 및 서버 연동

--- src/main/java/springboot/web/IndexController.java
+++ src/main/java/springboot/web/IndexController.java
@@ -1,11 +1,20 @@
 package springboot.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
+@Controller
 public class IndexController {
     @GetMapping("/")
     public String index() {
         //index.mustache 와 mapping
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
