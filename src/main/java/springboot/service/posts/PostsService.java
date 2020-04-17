8ab1bd1063cb4de64bf953b8b65a938d97d4be6e
yebink <yebin825@gmail.com>
2020-04-17 23:52:00 +0900

    게시글 목록 출력 기능 구현

--- src/main/java/springboot/service/posts/PostsService.java
+++ src/main/java/springboot/service/posts/PostsService.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import springboot.domain.posts.Posts;
 import springboot.domain.posts.PostsRepository;
+import springboot.web.dto.PostsListResponseDto;
 import springboot.web.dto.PostsResponseDto;
 import springboot.web.dto.PostsSaveRequestDto;
 import springboot.web.dto.PostsUpdateRequestDto;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/springboot/web/IndexController.java
+++ src/main/java/springboot/web/IndexController.java
@@ -2,14 +2,20 @@ package springboot.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import springboot.service.posts.PostsService;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
         //index.mustache 와 mapping
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
