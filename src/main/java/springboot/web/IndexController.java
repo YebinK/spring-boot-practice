2acaacd229184707d234233f392f42f116b38663
yebink <yebin825@gmail.com>
2020-04-18 02:19:11 +0900

    게시글 수정 기능 구현

--- src/main/java/springboot/web/IndexController.java
+++ src/main/java/springboot/web/IndexController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import springboot.service.posts.PostsService;
+import springboot.web.dto.PostsResponseDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -14,13 +16,20 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
-        //index.mustache 와 mapping
         model.addAttribute("posts", postsService.findAllDesc());
-        return "index";
+        return "index"; //index.mustache 와 mapping
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String update(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
