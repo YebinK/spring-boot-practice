52552a46687749014418f17f0adda69b7dd18ebe
yebink <yebin825@gmail.com>
2020-04-26 04:02:22 +0900

    네이버 로그인 연동 기능 구현

--- src/main/java/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/springboot/config/auth/dto/OAuthAttributes.java
@@ -25,17 +25,9 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId))
+            return ofNaver("id", attributes);
         return ofGoogle(userNameAttributeName, attributes);
-    }
-
-    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
-        return OAuthAttributes.builder()
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("email"))
-                .picture((String) attributes.get("picture"))
-                .attributes(attributes)
-                .nameAttributeKey(userNameAttributeName)
-                .build();
     }
 
     public User toEntity() {
@@ -44,6 +36,26 @@ public class OAuthAttributes {
                 .email(email)
                 .picture(picture)
                 .role(Role.GUEST)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return buildAttributes(userNameAttributeName, response, "profile_image");
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return buildAttributes(userNameAttributeName, attributes, "picture");
+    }
+
+    private static OAuthAttributes buildAttributes(String userNameAttributeName, Map<String, Object> response, String profile_image) {
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get(profile_image))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
                 .build();
     }
 }
